462ae96f2cb26fbfac261168719286c2b8241d00
alef6 <133623940+alef6@users.noreply.github.com>
2023-05-15 11:50:25 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-aquive="X-UA-Compatible" content="IE=edge">
+    <meta name="Viewport" content="Width=device-width,initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="proses.php">
+        <div> nama : <input type="text" name="nama"></div>
+        <div> umur :<input type="number" name="umur"></div>
+        <div> jurusan:<input type="text" name="jurusan"></div>
+        <button type="submit">kirim</button>
+    </form>
+</body>
+</html>
